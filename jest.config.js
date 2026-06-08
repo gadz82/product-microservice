08886fa71c55fd34fab8c54fd90bf296aac76e91
Francesco Marchesini <francesco.marchesini@docebo.com>
2026-06-08 13:24:02 +0200

    chore: add Jest configuration for unit testing
    add Jest configuration for unit testing

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('jest').Config} */
+const config = {
+	moduleFileExtensions: ['js', 'json', 'ts'],
+	rootDir: 'src',
+	testRegex: '.*\\.spec\\.ts$',
+	transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+	collectCoverageFrom: [
+		'**/*.(t|j)s',
+		'!**/*.spec.ts',
+		'!**/index.ts',
+		'!main.ts',
+		'!**/*.module.ts',
+		'!config/configuration.ts',
+		'!database/**/*',
+		'!common/config/configuration.ts'
+	],
+	coverageDirectory: '../coverage',
+	testEnvironment: 'node'
+};
+
+module.exports = config;
